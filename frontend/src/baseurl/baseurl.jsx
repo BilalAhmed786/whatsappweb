abe9e43a07436c139497e2be6de9f04fc6526ca5
BilalAhmed786 <fasst.sallar@gmail.com>
2026-09-18 11:53:32 +0500

    deploy

--- frontend/src/baseurl/baseurl.jsx
+++ frontend/src/baseurl/baseurl.jsx
@@ -1,2 +1,2 @@
-export const frontendbaseurl = "https://whatsappweb.fasst-sallar.workers.dev/"
+export const frontendbaseurl = "https://whatsappweb.fasst-sallar.workers.dev"
 export const backendbaseurl = "https://whatsappweb-backend-nksz.onrender.com"
